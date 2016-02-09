45451042c1368665ff53e965463a6b0e05731062
Kazunori OYA <terje@oya3.net>
2016-02-09 19:33:29 +0900

    各batファイルにタイトルを付加

--- samples/01_both_tcp/tcp_client/execute.bat
+++ samples/01_both_tcp/tcp_client/execute.bat
@@ -1,2 +1,3 @@
+title tcp_client
 pry -r "./execute_stream_simulator.rb"
 
--- samples/01_both_tcp/tcp_client/execute_with_less.bat
+++ samples/01_both_tcp/tcp_client/execute_with_less.bat
@@ -1,3 +1,4 @@
+title tcp_client(+less)
 touch stream_simulator.log
 start less +F stream_simulator.log
 pry -r "./execute_stream_simulator.rb"
